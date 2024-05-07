5b7bec27526f46e8cf9cae55031e2b7eb69bcc86
Ionut-Alexandru Oprea <91005312+10nutz@users.noreply.github.com>
2024-05-07 14:51:18 +0300

    Resolved bugs, added tables.

--- src/app/pages/bookings/bookings.component.ts
+++ src/app/pages/bookings/bookings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Booking } from 'src/app/models/Booking';
+import { BookingData } from 'src/app/models/BookingData';
 import { BookingService } from 'src/app/services/booking.service';
 import { DepartmentService } from 'src/app/services/department.service';
 import { DeskService } from 'src/app/services/desk.service';
@@ -13,8 +14,12 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class BookingsComponent implements OnInit {
   employeeEmail: string;
   bookings: Booking[];
+  currentPage = 1;
+  itemsPerPage = 5;
+  totalPages: number;
 
   selectedBookings: Booking[] = [];
+  paginatedBookings: Booking[];
 
   constructor(private bookingService: BookingService, private employeeService: EmployeeService, private deskService: DeskService, private departmentService: DepartmentService){
 
@@ -24,13 +29,31 @@ export class BookingsComponent implements OnInit {
     this.loadBookings();
   }
 
+  updatePaginatedBookings() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedBookings = this.bookings.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.updatePaginatedBookings();
+  }
+
+  prevPage() {
+    this.currentPage--;
+    this.updatePaginatedBookings();
+  }
+
+
   loadBookings() {
     this.employeeService.getEmployeeInfo().subscribe(response => {
       this.employeeEmail = response.email;
 
       this.bookingService.getActiveBookingsByEmployeeEmail(this.employeeEmail).subscribe(bookings => {
         this.bookings = bookings;
-
+        this.totalPages = Math.ceil(this.bookings.length / this.itemsPerPage);
+        this.updatePaginatedBookings();
         this.bookings.forEach(booking => {
           booking.startDate = booking.startDate.split('T')[0];
           booking.endDate = booking.endDate.split('T')[0];
@@ -57,7 +80,30 @@ export class BookingsComponent implements OnInit {
 
   cancelSelectedBooking() {
     this.selectedBookings.forEach(booking => {
-      this.bookingService.cancelBooking(booking.id).subscribe()});
+      this.bookingService.cancelBooking(booking.id).subscribe((response: string) =>{
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0); 
+
+        const bookingStartDate = new Date(booking.startDate);
+        bookingStartDate.setHours(0, 0, 0, 0);
+
+        if(currentDate >= bookingStartDate){
+
+          const startDate = new Date(booking.startDate);
+          currentDate.setDate(currentDate.getDate() + 1);
+          const endDate = currentDate;
+
+          const newBooking: BookingData = {
+            employeeId: booking.employeeId,
+            startDate: startDate.toISOString().split('T')[0],
+            endDate: endDate.toISOString().split('T')[0],
+            deskId: booking.deskId
+          };
+
+          this.bookingService.addBooking(newBooking).subscribe();
+        }
+      }
+      )});
 
     this.selectedBookings = [];
     window.location.reload();
--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -10,35 +10,56 @@ import { EmployeeService } from 'src/app/services/employee.service';
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent implements AfterViewInit{
+export class HistoryComponent implements AfterViewInit {
   employeeEmail: string;
+  currentPage = 1;
+  itemsPerPage = 5;
+  totalpages: number;
   bookings: Booking[];
+  paginatedBookings: Booking[];
 
-  constructor(private bookingService: BookingService, private employeeService: EmployeeService, private deskService: DeskService, private departmentService: DepartmentService){
+  constructor(private bookingService: BookingService, private employeeService: EmployeeService, private deskService: DeskService, private departmentService: DepartmentService) {
 
   }
-  
+
   ngAfterViewInit(): void {
     this.employeeService.getEmployeeInfo().subscribe(response => {
       this.employeeEmail = response.email;
 
       this.bookingService.getBookingHistoryByEmployeeEmail(this.employeeEmail).subscribe(bookings => {
         this.bookings = bookings;
-
+        this.totalpages = Math.ceil(this.bookings.length / this.itemsPerPage);
+        this.updatePaginatedBookings();
         this.bookings.forEach(booking => {
           booking.startDate = booking.startDate.split('T')[0];
           booking.endDate = booking.endDate.split('T')[0];
-          
+
           this.deskService.getDeskById(booking.deskId.toString()).subscribe(desk => {
             booking.desk = desk;
 
             this.departmentService.getDepartmentById(desk.departmentId).subscribe(department => {
               booking.department = department;
             });
-          });
-        });
+          })
+        })
       });
     });
+  }
+
+  updatePaginatedBookings() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedBookings = this.bookings.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.updatePaginatedBookings();
+  }
+
+  prevPage() {
+    this.currentPage--;
+    this.updatePaginatedBookings();
   }
 
 }
